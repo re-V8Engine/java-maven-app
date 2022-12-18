2ff2de2acd32ec991d1e82a0d8cd82a89978927e
re-V8Engine <37089981+re-V8Engine@users.noreply.github.com>
2022-12-18 14:50:54 +0300

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,14 +1,22 @@
 def buildApp() {
-    echo "Building application..."
-    echo "Building version ${APP_VERSION}..."
+    echo "Building jar..."
+    sh 'mvn package'
 }
 
 def testApp() {
-    echo 'Testing application...'
+    sh 'mvn test'
 }
 
-def deployApp() {
-    echo 'Deploying application...'
-    echo "Deploying version ${params.VERSION}..."
+def buildImage() {
+    echo "Building docker image..." 
+    withCredentials([usernamePassword(credentialsId: 'dockerhub-credentials', passwordVariable: 'PASSWORD', usernameVariable: 'USERNAME')]) {
+        sh "docker build -t v8engine/java-maven-app:1.4.0 ."
+        sh "echo ${PASSWORD} | docker login -u ${USERNAME} --password-stdin"
+        sh "docker push v8engine/java-maven-app:1.4.0"
+    }
+}
+
+def deploy() {
+    echo "Deploying..."
 }
 return this
