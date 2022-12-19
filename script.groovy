449ceaf9c2dc39f51976841de8e89cc731c145c6
V8Engine <alx0044@gmail.com>
2022-12-19 18:00:56 +0300

    JSL used in Jenkinsfile

--- script.groovy
+++ script.groovy
@@ -1,19 +1,5 @@
-def buildApp() {
-    echo "Building jar..."
-    sh 'mvn package'
-}
-
 def testApp() {
     sh 'mvn test'
-}
-
-def buildImage() {
-    echo "Building docker image..." 
-    withCredentials([usernamePassword(credentialsId: 'dockerhub-credentials', passwordVariable: 'PASSWORD', usernameVariable: 'USERNAME')]) {
-        sh "docker build -t v8engine/java-maven-app:1.4.0 ."
-        sh "echo ${PASSWORD} | docker login -u ${USERNAME} --password-stdin"
-        sh "docker push v8engine/java-maven-app:1.4.0"
-    }
 }
 
 def deploy() {
